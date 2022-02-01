ead08eb4d6b8df18df67f6eea9d3a97f9ba27658
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-02-01 20:56:35 +0500

    added recipe schema and import it in schema.js
    added recipe schema that references both schemas i.e Chef and Ingredient.

--- recipestudio/schemas/schema.js
+++ recipestudio/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import chef from "./chef"
 import ingredient from './ingredient'
+import recipe from './recipe'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -16,5 +17,6 @@ export default createSchema({
     /* Your types here! */
     chef,
     ingredient,
+    recipe,
   ]),
 })
