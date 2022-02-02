b4edf5eaf1327bcc77c12df7f0bc23bc5e9628f1
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-02-02 15:11:17 +0500

    complete first page template in index.js
    fetch data using QROQ query from vercel and shown and also change data format in apiversion in sanity.json

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,43 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { sanityClient, urlFor } from "../lib/sanity"
 
-export default function Home() {
+const recipesQuery = `*[_type == "recipe"]{
+  _id,
+  name,
+  slug,
+  mainImage
+}`
+
+export default function Home({ recipes }) {
+  console.log(13, recipes)
   return (
     <div>
       <Head>
-        <title>Intro Next App</title>
+        <title>Shahid's Kitchen🍍</title>
       </Head>
+      <h1>Welcome to Shahid's Kitchen🍍</h1>
+      <ul className='recipes-list'>
+        {recipes?.length > 0 && recipes.map((recipe) => (
+          <li key={recipe._id} className='recipe-card'>
+            <Link href="/">
+            <a>
+              <img src={urlFor(recipe.mainImage).url()} />
+              <span>{recipe.name}</span>
+            </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const recipes = await sanityClient.fetch(recipesQuery)
+  return {
+    props: {
+      recipes: recipes,
+    }
+  }
 }
